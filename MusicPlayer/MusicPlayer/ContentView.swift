f6cc1b6c390cdcf1a184ad10b42aeab5623bdea4
Antoine <bababouch07@gmail.com>
2020-10-31 23:59:07 +0100

    part 2

--- MusicPlayer/MusicPlayer/ContentView.swift
+++ MusicPlayer/MusicPlayer/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                 ScrollView(.horizontal, showsIndicators: false) {
                     LazyHStack {
                         ForEach(albums) { album in
-                            AlbumArt(album: album).onTapGesture {
+                            AlbumArt(album: album, displayName: true).onTapGesture {
                                 currentAlbum = album
                             }
                         }
@@ -27,7 +27,7 @@ struct ContentView: View {
                 
                 LazyVStack {
                     ForEach(currentAlbum?.songs ?? albums.first!.songs) { song in
-                        SongCell(song: song)
+                        SongCell(album: currentAlbum ?? albums.first!, song: song)
                     }
                 }
             }
@@ -37,16 +37,19 @@ struct ContentView: View {
 
 struct AlbumArt: View {
     var album: Album
+    var displayName: Bool
     
     var body: some View {
         ZStack(alignment: .bottom) {
             Image(album.image).resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 170, height: 200, alignment: .center)
-            ZStack {
-                Rectangle().fill(Color.black).opacity(0.5)
-                Text(album.name).foregroundColor(.white)
-            }.frame(height: 60, alignment: .center)
+            if displayName {
+                ZStack {
+                    Rectangle().fill(Color.black).opacity(0.8)
+                    Text(album.name).foregroundColor(.white)
+                }.frame(height: 60, alignment: .center)
+            }
         }.clipped()
         .cornerRadius(20)
         .shadow(radius: 10)
@@ -55,17 +58,20 @@ struct AlbumArt: View {
 }
 
 struct SongCell: View {
+    var album: Album
     var song: Song
     
     var body: some View {
-        HStack {
-            ZStack {
-                Circle().fill(Color.blue).frame(width: 50, height: 50)
-                Circle().fill(Color.white).frame(width: 20, height: 20)
-            }
-            Text(song.name).bold()
-            Spacer()
-            Text(song.duration)
-        }.padding(20)
+        NavigationLink(destination: PlayerView(album: album, song: song)) {
+            HStack {
+                ZStack {
+                    Circle().fill(Color.blue).frame(width: 50, height: 50)
+                    Circle().fill(Color.white).frame(width: 20, height: 20)
+                }
+                Text(song.name).bold()
+                Spacer()
+                Text(song.duration)
+            }.padding(20)
+        }.buttonStyle(PlainButtonStyle())
     }
 }
--- MusicPlayer/MusicPlayer/PlayerView.swift
+++ MusicPlayer/MusicPlayer/PlayerView.swift
@@ -8,13 +8,67 @@
 import SwiftUI
 
 struct PlayerView: View {
+    var album: Album
+    var song: Song
+    @State var isPlaying: Bool = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(album.image)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            Rectangle()
+                .fill(Color.black)
+                .opacity(0.8)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                AlbumArt(album: album, displayName: false)
+                Text(song.name)
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .fontWeight(.light)
+                Spacer()
+                ZStack {
+                    Color.white
+                        .cornerRadius(20)
+                        .shadow(radius: 10)
+                    HStack {
+                        Button(action: previous) {
+                            Image(systemName: "arrow.left.circle")
+                                .resizable()
+                        }.frame(width: 50, height: 50, alignment: .center)
+                        .foregroundColor(.black)
+                        .opacity(0.2)
+                        
+                        Button(action: playPause) {
+                            Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                                .resizable()
+                        }.frame(width: 70, height: 70, alignment: .center)
+                        
+                        Button(action: next) {
+                            Image(systemName: "arrow.right.circle")
+                                .resizable()
+                        }.frame(width: 50, height: 50, alignment: .center)
+                        .foregroundColor(.black)
+                        .opacity(0.2)
+                    }
+                }.edgesIgnoringSafeArea(.all)
+                .frame(height: 200, alignment: .center)
+            }
+            
+        }
     }
-}
-
-struct PlayerView_Previews: PreviewProvider {
-    static var previews: some View {
-        PlayerView()
+    
+    func playPause() {
+        isPlaying.toggle()
+    }
+    
+    func next() {
+        
+    }
+    
+    func previous() {
+        
     }
 }
